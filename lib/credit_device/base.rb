f88aafa0e2caf51bcb26bb947c0bba78f2cbb4a5
Rodrigo Barreto <rodrigonbarreto@gmail.com>
2018-08-05 17:10:46 +0200

    Update base model, and README.md

--- lib/credit_device/base.rb
+++ lib/credit_device/base.rb
@@ -1,5 +1,11 @@
 module CreditDevice
   class Base
-
+    def self.config(url, username, password)
+      CreditDevice.configure do |config|
+        config.url = url
+        config.username = username
+        config.password = password
+      end
+    end
   end
 end
